78748396cc48198906bcd5216d452e482f34ce16
Hakeem <hakeem.trotmanp1@gmail.com>
2020-09-01 15:58:51 -0400

    Pending Lab1

--- Labs/Lab1.cpp
+++ Labs/Lab1.cpp
@@ -1,1 +1,47 @@
-// Pending
+// Pending: Ill Get this finished in a day or two
+//In class
+
+
+//Take Home
+
+//A private static double array field consisting of 3 elements.
+
+//A public default constructor that assigns 0 to each element of the field
+
+//A public overloaded constructor that takes three double parameters. It assigns the parameters to the respective
+//elements of the field.
+
+
+//A public copy constructor
+
+//A public assignment operator.
+
+
+//A public empty destructor.
+
+//A public double constant method named GetX() that takes no parameters. It returns the first element of the
+// field.
+
+
+//  A public double constant method named GetZ() that takes no parameters. It returns the third element of the
+// field.
+
+
+//  A public void method named SetX() that takes a double parameter. It assigns the parameter to the first element
+// of the field.
+
+
+//  A public void method named SetY() that takes a double parameter. It assigns the parameter to the second
+// element of the field.
+
+
+//  A public void method named SetZ() that takes a double parameter. It assigns the parameter to the third element
+// of the field.
+
+
+//  A public string constant method named ToString() that takes no parameters. It returns a string in the format
+// ''(x ,y ,z )'' where x , y, z are the elements of the field representing their respective coordinate. Furthermore,
+// they must be contain exactly two decimal points.
+
+
+//  A friend overloaded ostream operator. It displays the elements of the field in the same format as ToString().
