8badf9525f24acbea1667b39f2082f5d9a4c27b1
Hakeem <hakeem.trotmanp1@gmail.com>
2020-10-28 17:17:58 -0400

    ProjectA Update 2

--- Projects/ProjectA.h
+++ Projects/ProjectA.h
@@ -1,6 +1,12 @@
-//due 27 of october
+//due 29 of october
+//interger field means to move information into a linklist
+//Ex: number 123 covers 3 Nodes.
+//possitve in nodes
 
-
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "Node.h"
 
 
 template<class T>
@@ -8,17 +14,26 @@ class Integer{
 
     private:
     //A private linked list field that is used to represent the integer.
-    Node<int>* t = new Node<int>;
-    t->data = data;
-    t->link= NULL;
+    Node<T>* integerNode = new Node;
+    // t->data = data;
+    // t->link= NULL;
     //return t
-    integer<int>;
+
+
+
+    // int data;
+    // Node* next;
+    // int integerNode;
+
 
 
 
     public:
     //A public default constructor that initializes the integer field to 0.
-        Integer(integer = 0){}
+        Integer(){
+            integerNode= 0;//Sets linkedlist to 0
+    
+        }
 
     /*    A public overloaded constructor that takes a string parameter. It converts the parameter and assigns
     it to the integer field if parameter is in the proper real number integer format. Otherwise, it initialize
@@ -28,58 +43,147 @@ class Integer{
         bool -> char -> short int -> int ->
         unsigned int -> long -> unsigned -> 
         long long -> float -> double -> long double
+
+
+    //Step 1) Convert string into an int
+    //Step 2) Link coverted varible into node(s) Ex: 7203 makes a node per digit, so i have 4 nodes now
+    //      if i can not (no string), initialize to 0;
+    //Step 3) Return new node or rather the new linklist
     */
-        Interger(stringstream::string interger){
-            int numberInt= 0;// Tracks place of number Ex: 1's, 10's 100's places
-            while(interger->data >= '0' && interger->data != NULL){ //while the string (interger) >= string 0 
-                numberInt = interger '0';
-                if(interger->data >= 0 !! interger->data < 0){// if the data is a real number( covers inf to -inf)
-                    //I need something to deal with doubles
-                    numberInt = interger->data + '0'; // Type conversion makes the data of the node a interger
-                    t->data->link = numberInt;  // Changes the data(rather than the node) into the newly converted information
-                    t = t->next;    //traverses the linklist
-                    interger = interger->next;  //traverses the linklist
-                }else{  // initialize the integer field to 0
-                    t->link = 0; //Set first node to 0(WIP)
-                    t->next->link = NULL;//Sets next node NULL so the while loop terminates
+        Integer(std::string integer){
+    
+            int strToInt = 0;
+           
+        
+                //this should create a node for each number a string has, and skip non-number charcters
+            for(int i = 0; integer.length(); i++){
+            if(strToInt > 0 || strToInt <= 0){//Checks if number is a negative or postive number
+
+                strToInt = integer[i] - '0';//strToInt is set to the interger of the current index
+                Insert(integerNode,integerNode, strToInt); //inserts converted string into linklist: This should also fill in the first node
+                Create(integerNode);//creates a new node for linkedlist t;
+                integerNode = integerNode->next;//goes to created node;
+                    
+                }else{//anything other than a number triggers this else
+                    i++;//will increase i once basically skipping that char
                 }
-            }
-            return t;//returns  new linklist 
+
+            } 
+            return integerNode = 0; //initializes list to 0
         }
 
         /*A public overloaded constructor that takes an int parameter. It converts the parameter and assigns it
     to the integer field.
 
-        **I assume this is a short int to integer(int) conversion**
+        If peremter is an Int, i sould assign it to a linkedlist 
+            Link coverted varible into node(s) Ex: 7203 makes a node per digit, so i have 4 nodes now
+
     */
-        Interger(int interger){
-            
-          
+        Integer(int integer){
+            int digitArray[]={};
+                //counts digit to give length
+            int n = integer;
+            int digitCount = 0;
+
+            while(n != 0){
+                n = n/10;
+                digitCount++;//this is used to find the length without changing the oringnal integer
+            }
+
+            for (int i = 0; i <= digitCount; i++) {//Changes an int into an array, but its backwards
+                //ex: num = 12345. This gives array[] = {5, 4, 3, 2, 1}
+                digitArray[i] = integer % 10;
+                integer /= 10;
+            }
+
+            //Next i transfer the contents of the array into nodes, accounting for
+            //the number being printed backwards
+            int CT = digitCount;
+            while(CT != 0 && integerNode->next != NULL){//If CT = 0 and the linkedlist is not empty the array will be inserted
+
+                Insert(integerNode ,integerNode ,digitArray[CT]);//Inserts current index into linkedlist t
+                Create(integerNode);//creates a new node for linkedlist t;
+                integerNode = integerNode->next;//goes to created node;
+                CT--;
+            }
+
+
         }
 
-
+       
         /*A public overloaded constructor that takes an double parameter. It converts the parameter and assigns
-    it to the integer field. When converting a double to an int, the decimal portion is truncated.*/
+    it to the integer field. When converting a double to an int, the decimal portion is truncated(rounded).*/
 
-        Interger(double interger){
-            while(interger->next != NULL){
-                convert<int> = interger->data/100; //converts the double node of interger and makes it an interger
-                t->link = convert; // links t's node with the converted one
-                t=t->next;
-                interger=interger->next;// node t should be as long as node interger
+        Integer(double integer){
+            int doubleToInt = integer - 0;//due to type casting int has more priority than double, so double is changed to int 
+            //aside from this first bit, this shoul look like Integer(int integer)
+
+            int digitArray[]={};
+                //counts digit to give length
+            int n = doubleToInt;
+            int digitCount = 0;
+
+            while(n != 0){
+                n = n/10;
+                digitCount++;//this is used to find the length without changing the oringnal integer
             }
-            return t;
+
+            for (int i = 0; i <= digitCount; i++) {//Changes an int into an array, but its backwards
+                //ex: num = 12345. This gives array[] = {5, 4, 3, 2, 1}
+                digitArray[i] = doubleToInt % 10;
+                doubleToInt /= 10;
+            }
+
+            //Next i transfer the contents of the array into nodes, accounting for
+            //the number being printed backwards
+            int CT = digitCount;
+            while(CT != 0 && integerNode->next != NULL){//If CT = 0 and the linkedlist is not empty the array will be inserted
+
+                Insert(integerNode ,integerNode ,digitArray[CT]);//Inserts current index into linkedlist t
+                Create(integerNode);//creates a new node for linkedlist t;
+                integerNode = integerNode->next;//goes to created node;
+                CT--;
+            }
+
+
+
         }
     //A public copy constructor.
-        Integer(const Integer& obj);
+        Integer(const Integer& obj){
+            //Am I supposed to turn the linkedlist into an Obj or the all nodes
+            //all nodes into obj
+            while(integerNode->next != NULL){
+                integerNode = integerNode->next;
+                integerNode = obj.integerNode->data;//will turn current node into an obj
+            }
+            /*
+                //linked-list into obj
+                t= obj.t;
+            */
+        }
     //A public assignment operator.
-        Integer& operator= (const Rational& rhs);
+        Integer& operator= (const Integer& rhs){
+            //Am I supposed to turn the linkedlist into an rhs or the all nodes
+            //all nodes into rhs
+            while(integerNode->next != NULL){
+                integerNode = integerNode->next;
+                integerNode = rhs.integerNode->data;//will turn current node into an rhs
+            }
+            /*
+                //linked-list into rhs
+                t= rhs.t;
+            */
+        }
+        
+
     //A public destructor. It deallocates the integer field.
-        ~Integer(int Integer);
+        ~Integer();
         /*A friend overloaded addition operator that takes two constant integer reference parameters. It returns
     an integer object that is equal to the sum of the two parameters.*/
-        friend Addition& operator(T& int1, T& int2){
-            //check if varibles are ints
+        friend std::ostream& operator Addition(T& int1, T& int2){
+           //Since these int1 and int2 are references I assume both come from nodes
+            
+
 
             //Addition
             int solution= int1 + int2;
@@ -88,7 +192,8 @@ class Integer{
         /*A friend overloaded subtraction operator that takes two constant integer reference parameters. It
     returns an integer object that is equal to the difference of the first parameter minus the second pa-
     rameter.*/
-        friend Subtraction& operator(T& int1, T& int2){
+        friend std::ostream& operator Subtraction(T& int1, T& int2){
+            //Since these int1 and int2 are references I assume both come from nodes
 
             int difference = int1 - int2;
 
@@ -102,16 +207,20 @@ class Integer{
         /*A public constant method named ToString() that takes no parameter. It returns a string represen-
     tation of the integer field.*/
         std::string ToString(){
-            stringstream out;
-            out <<"The addition of the chosen numbers is: "<< Addition<<".\n";
-            out <<"The difference of the chosen numbers is: "<< Subtraction<<".";
+            std::stringstream out;
+            out <<"The addition of the chosen numbers is: "<< solution<<".\n";
+            out <<"The difference of the chosen numbers is: "<< difference<<".";
             return out.str();
         }
     // A friend overloaded ostream operator. It displays the integer field.
         friend ostream& operator(ostream& const Interger& obj){
-            Integer a;
-            cout << a.ToString();
-            cout<< a;
+            stringstream out;
+            while(integerNode->next != NULL){
+                integerNode = integerNode->next;
+                out << integerNode->data;
+
+            }
+            return out.str();
         }
     // A friend overloaded istream operator. It reads into the inetger field.
         friend ostream& operator(){
