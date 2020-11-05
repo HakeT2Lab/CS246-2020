f8ac8f7b243d4376242e71bbd5645f576b6e362c
Hakeem <hakeem.trotmanp1@gmail.com>
2020-11-05 16:22:31 -0500

    WIP Lab8A.cpp

--- Labs/Lab08/lab8A.cpp
+++ Labs/Lab08/lab8A.cpp
@@ -3,7 +3,33 @@
 #include "Vector.h"
 using ulong = unsigned long;
 
+
+/*
+It returns the greatest common divisors of the elements of data where the greatest common divisor is the greatest
+number that can divide all the elements evenly. If data is empty, it returns 0. You can only use functions you define to
+help define GCD().
+
+*/
+
+ulong GCD(ds::Vector<ulong>& data){
+  std::cout<<"\nActive\n";
+  if(data.Length() != 0 ){
+    for(int i =0; i< data.Length(); i++){
+      if(){
+        
+      }
+    }
+  }
+
+
+
+  return 0;
+}
+
 int main()
 {
+  ds::Vector<ulong> Data;
+  GCD(Data);
+
 	return 0;
 }
